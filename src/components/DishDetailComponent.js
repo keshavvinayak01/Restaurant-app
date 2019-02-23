8aa106a1ced34aa3b3555eee0680dc8879fd51fc
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-02-23 13:51:56 +0530

    React Animations

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -5,31 +5,37 @@ import {Control,LocalForm,Errors} from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import '../App.css';
 import { Loading } from './LoadingComponent';
-import { baseUrl } from '.../shared/baseUrl'
+import { baseUrl } from '.../shared/baseUrl';
+import {FadeTransform,Fade,Stagger} from 'react-animation-components';
 // Validators for form
 	function RenderDish({dish,comments}){
 		if (dish != null) {
 			return(
-				<div className= 'selected'>
-				<div className = "col-12 col-md-5 mt-1 image">
-				<Card>
-					<CardImg object src = {baseUrl + dish.image} alt = {dish.name} />
-					<CardBody>
-						<CardTitle>
-							{dish.name}
-						</CardTitle>
-						<CardText>
-							{dish.description}
-						</CardText>	
-					</CardBody>
-				</Card>
-				</div>
-				<div className = "col-12 col-md-5 mt-1">
-				<Card>
-				<RenderComments comments={comments} />
-				</Card>
-				</div>
-				</div>
+				<FadeTransform in 
+                transformProps = {{
+                    exitTransform : 'scale(0.5) translateX(100%)'
+                }}>
+					<div className= 'selected'>
+					<div className = "col-12 col-md-5 mt-1 image">
+					<Card>
+						<CardImg object src = {baseUrl + dish.image} alt = {dish.name} />
+						<CardBody>
+							<CardTitle>
+								{dish.name}
+							</CardTitle>
+							<CardText>
+								{dish.description}
+							</CardText>	
+						</CardBody>
+					</Card>
+					</div>
+					<div className = "col-12 col-md-5 mt-1">
+					<Card>
+					<RenderComments comments={comments} />
+					</Card>
+					</div>
+					</div>
+				</FadeTransform>
 				);
 		}
 		else{
@@ -42,18 +48,25 @@ import { baseUrl } from '.../shared/baseUrl'
 		if(comments.length){
 			const comments_render = comments.map((comment) => {
 				return (
-						<div>	
+						<Fade in>
+						<li key={comment.id} >
 						<p>{comment.comment}</p>
 						<p>-- {comment.author}, {new Intl.DateTimeFormat('en-US',
 							{year:'numeric',month:'short',day:'2-digit'})
 						.format(new Date (Date.parse(comment.date)))}</p>
-						</div>
+						</li>
+						</Fade>
+						
 				)
 			});
 			return(
 				<div className="comments">
 				<h4>Comments</h4>
+				<Stagger in>
+				<ul className = "list-unstyled">
 				{comments_render}
+				</ul>
+				</Stagger>
 				</div>
 				)
 		}
